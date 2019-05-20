5e62939f25436995014ed42bf6e880bcf7809bf6
shubhamstpt22 <46398550+shubhamstpt22@users.noreply.github.com>
2019-05-20 12:37:14 +0530

    Update constants.js

--- module3/app/constants.js
+++ module3/app/constants.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular.module('NarrowItDownApp')
-    .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
 })();
